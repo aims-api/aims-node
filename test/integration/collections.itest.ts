883b2b842e48f55d71c009ea58ad2f683a9f10de
afatoga <afatoga@gmail.com>
2024-08-19 18:12:19 +0200

    all query tests + separate files

--- test/integration/collections.itest.ts
+++ test/integration/collections.itest.ts
@@ -1,63 +1,6 @@
 import { describe, expect, test, beforeAll } from '@jest/globals'
 import { Client } from '../../src/client'
 
-describe('Autocomplete endpoint', () => {
-  test('success, returns data', async () => {
-    const testClient = new Client({
-      authorization: process.env.TEST_SECRET_TOKEN ?? '',
-    })
-
-    const response = await testClient.endpoints.autocomplete('test')
-
-    expect(response).toMatchObject({
-      success: true,
-    })
-  })
-})
-
-describe('Query byUrl endpoint', () => {
-  test('success, returns data', async () => {
-    const testClient = new Client({
-      authorization: process.env.TEST_SECRET_TOKEN ?? '',
-    })
-
-    const response = await testClient.endpoints.query.byUrl({ link: 'https://youtu.be/D8K90hX4PrE' })
-
-    expect(response).toMatchObject({
-      success: true,
-    })
-  })
-})
-
-describe('Query byAudioFile endpoint', () => {
-  test('error, file not provided', async () => {
-    const testClient = new Client({
-      authorization: process.env.TEST_SECRET_TOKEN ?? '',
-    })
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const response = await testClient.endpoints.query.byAudioFile({ track: {} as any })
-
-    expect(response).toMatchObject({
-      success: false,
-    })
-  })
-})
-
-describe('Unified search endpoint', () => {
-  test('success, returns data', async () => {
-    const testClient = new Client({
-      authorization: process.env.TEST_SECRET_TOKEN ?? '',
-    })
-
-    const response = await testClient.endpoints.search({ query: 'gondolas in venice' })
-
-    expect(response).toMatchObject({
-      success: true,
-    })
-  })
-})
-
 describe('Project manipulation', () => {
   let testClient: Client
   const projectKey = `pipeline-test-${Date.now()}`
---
+++ test/integration/query.itest.ts
@@ -0,0 +1,97 @@
+import { describe, expect, test, beforeAll } from '@jest/globals'
+import { Client } from '../../src/client'
+
+describe('Search endpoints', () => {
+  let testClient: Client
+
+  beforeAll(() => {
+    testClient = new Client({
+      authorization: process.env.TEST_SECRET_TOKEN ?? '',
+    })
+  })
+
+  test('Unified search', async () => {
+    const response = await testClient.endpoints.search({ query: 'gondolas in venice' })
+
+    expect(response).toMatchObject({
+      success: true,
+    })
+  })
+
+  test('QueryByUrl', async () => {
+    const response = await testClient.endpoints.query.byUrl({ link: 'https://youtu.be/D8K90hX4PrE' })
+
+    expect(response).toMatchObject({
+      success: true,
+    })
+  })
+
+  test('QueryByFileUrl, valid file', async () => {
+    const response = await testClient.endpoints.query.byFileUrl({
+      track: 'https://download.samplelib.com/mp3/sample-3s.mp3',
+    })
+
+    expect(response).toMatchObject({
+      success: true,
+    })
+  })
+
+  test('QueryByAudioFile', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const response = await testClient.endpoints.query.byAudioFile({ track: {} as any })
+
+    expect(response).toMatchObject({
+      success: false,
+    })
+  })
+
+  test('QueryByAudioFileHash', async () => {
+    const response = await testClient.endpoints.query.byAudioFileHash({ hash: '123' })
+
+    expect(response).toMatchObject({
+      success: false,
+    })
+  })
+
+  test('QueryByText', async () => {
+    const response = await testClient.endpoints.query.byText({ text: 'rainbow' })
+
+    expect(response).toMatchObject({
+      success: true,
+    })
+  })
+
+  test('QueryByTextHash', async () => {
+    const response = await testClient.endpoints.query.byTextHash({ hash: '123' })
+
+    expect(response).toMatchObject({
+      success: false,
+    })
+  })
+
+  test('QueryById', async () => {
+    const response = await testClient.endpoints.query.byId({ track_id: '' })
+
+    expect(response).toMatchObject({
+      success: false,
+    })
+  })
+
+  test('QueryByIds', async () => {
+    const response = await testClient.endpoints.query.byIds({ track_ids: [] })
+
+    expect(response).toMatchObject({
+      success: false,
+    })
+  })
+
+  test('QueryByTag', async () => {
+    const response = await testClient.endpoints.query.byTag({ tag_id: 0 })
+
+    expect(response).toMatchObject({
+      success: false,
+    })
+  })
+
+  test
+})
