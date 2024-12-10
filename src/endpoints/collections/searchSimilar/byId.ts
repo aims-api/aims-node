96d34fccf547c237a2c5febfcbfc26e3c002cb2e
LV <lukas@sinesquare.com>
2024-12-10 13:51:51 +0100

    prettier run

--- src/endpoints/collections/searchSimilar/byId.ts
+++ src/endpoints/collections/searchSimilar/byId.ts
@@ -1,25 +1,20 @@
-import { AxiosInstance } from "axios";
-import { Response } from "../../../helpers/apiResponse";
-import { searchSimilar } from "./index";
-import {
-	SimilarCollectionsQueryParams,
-	SimilarCollectionsResponse,
-} from "../../../helpers/types/collection";
+import { AxiosInstance } from 'axios'
+import { Response } from '../../../helpers/apiResponse'
+import { searchSimilar } from './index'
+import { SimilarCollectionsQueryParams, SimilarCollectionsResponse } from '../../../helpers/types/collection'
 
 export interface SimilarSearchById {
-	data: {
-		id: string;
-		group_id?: string;
-		detailed?: boolean;
-	};
+  data: {
+    id: string
+    group_id?: string
+    detailed?: boolean
+  }
 }
 
 const byId =
-	(client: () => AxiosInstance, path: string) =>
-	async (
-		request: SimilarSearchById & SimilarCollectionsQueryParams,
-	): Promise<Response<SimilarCollectionsResponse>> => {
-		return await searchSimilar(client, path, "by-id", request);
-	};
+  (client: () => AxiosInstance, path: string) =>
+  async (request: SimilarSearchById & SimilarCollectionsQueryParams): Promise<Response<SimilarCollectionsResponse>> => {
+    return await searchSimilar(client, path, 'by-id', request)
+  }
 
-export { byId };
+export { byId }
--- src/endpoints/collections/searchSimilar/index.ts
+++ src/endpoints/collections/searchSimilar/index.ts
@@ -1,42 +1,31 @@
-import { AxiosInstance } from "axios";
-import { API_VERSION } from "../../../consts";
+import { AxiosInstance } from 'axios'
+import { API_VERSION } from '../../../consts'
+import { parseError, Response, successResponse } from '../../../helpers/apiResponse'
 import {
-	parseError,
-	Response,
-	successResponse,
-} from "../../../helpers/apiResponse";
-import {
-	SimilarCollectionsQueryParams,
-	SimilarCollectionsResponse,
-	similarCollectionsResponseSchema,
-	similarCollectionsResponseSchemaDetailed,
-} from "../../../helpers/types/collection";
-import { SimilarSearchByKey } from "./byKey";
-import { SimilarSearchById } from "./byId";
+  SimilarCollectionsQueryParams,
+  SimilarCollectionsResponse,
+  similarCollectionsResponseSchema,
+  similarCollectionsResponseSchemaDetailed,
+} from '../../../helpers/types/collection'
+import { SimilarSearchByKey } from './byKey'
+import { SimilarSearchById } from './byId'
 
 export const searchSimilar = async (
-	client: () => AxiosInstance,
-	path: string,
-	by: string,
-	request: (SimilarSearchByKey | SimilarSearchById) &
-		SimilarCollectionsQueryParams,
+  client: () => AxiosInstance,
+  path: string,
+  by: string,
+  request: (SimilarSearchByKey | SimilarSearchById) & SimilarCollectionsQueryParams,
 ): Promise<Response<SimilarCollectionsResponse>> => {
-	try {
-		const response = await client().post(
-			`/${API_VERSION}/${path}/similar/${by}`,
-			request.data,
-			{
-				params: request.params,
-			},
-		);
-		const detailed = request.data?.detailed ?? false;
-		const parserResponse = (
-			detailed
-				? similarCollectionsResponseSchemaDetailed
-				: similarCollectionsResponseSchema
-		).parse(response.data);
-		return successResponse(parserResponse);
-	} catch (error) {
-		return parseError(error);
-	}
-};
+  try {
+    const response = await client().post(`/${API_VERSION}/${path}/similar/${by}`, request.data, {
+      params: request.params,
+    })
+    const detailed = request.data?.detailed ?? false
+    const parserResponse = (
+      detailed ? similarCollectionsResponseSchemaDetailed : similarCollectionsResponseSchema
+    ).parse(response.data)
+    return successResponse(parserResponse)
+  } catch (error) {
+    return parseError(error)
+  }
+}
