3748f9dd72e125ded57174db7b3cd3ae4b4c72cb
afatoga <32682730+afatoga@users.noreply.github.com>
2025-02-18 11:05:42 +0100

    [AIMS-4146] export project (#58)
    * export collection
    
    * request path
    
    * exact error handling
    
    * response undefined

---
+++ src/endpoints/collections/export.ts
@@ -0,0 +1,34 @@
+import { AxiosInstance } from 'axios'
+import { API_VERSION } from '../../consts'
+import {
+  parseError,
+  successResponse,
+  Response,
+} from '../../helpers/apiResponse'
+import { AxiosError } from 'axios'
+
+// ANNOUNC: this type is used only by /src/client/index.ts endpoints
+export interface ExportCollection {
+  id: string
+}
+
+export const exportCollection =
+  (client: () => AxiosInstance, path: 'project', by: 'by-key' | 'by-id') =>
+  async (request: ExportCollection): Promise<Response<undefined>> => {
+    try {
+      await client().post(`/${API_VERSION}/${path}/export/${by}/${request.id}`)
+      return successResponse(undefined)
+    } catch (error) {
+      if (
+        error instanceof AxiosError &&
+        error.response?.status === 422 &&
+        (error.response?.data?.message ===
+          'Collection has already been exported!' ||
+          error.response?.data?.code === 1002)
+      ) {
+        // case: collection already exported
+        return successResponse(undefined)
+      }
+      return parseError(error)
+    }
+  }
