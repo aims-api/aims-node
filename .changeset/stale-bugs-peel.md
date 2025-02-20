f50efb10d606bcbbac513152b331119c66f5133d
afatoga <afatoga@gmail.com>
2025-02-20 16:56:57 +0100

    creating a collection with image

---
+++ .changeset/stale-bugs-peel.md
@@ -0,0 +1,5 @@
+---
+"@aims-api/aims-node": patch
+---
+
+creating collection with image
--- src/endpoints/collections/create.ts
+++ src/endpoints/collections/create.ts
@@ -1,7 +1,17 @@
 import { AxiosInstance } from 'axios'
-import { CollectionResponse, collectionResponseSchema } from '../../helpers/types/collection'
+import {
+  CollectionResponse,
+  collectionResponseSchema,
+} from '../../helpers/types/collection'
 import { API_VERSION } from '../../consts'
-import { parseError, successResponse, Response } from '../../helpers/apiResponse'
+import {
+  parseError,
+  successResponse,
+  Response,
+} from '../../helpers/apiResponse'
+import { ReadStream } from 'fs'
+import FormData from 'form-data'
+import { transformObjToFormData } from '../../helpers/utils'
 
 // ANNOUNC: this type is used only by /src/client/index.ts endpoints
 export interface CreateRequest {
@@ -10,13 +20,23 @@ export interface CreateRequest {
   description?: string
   track_ids?: Array<string>
   user_id?: string
+  image?: string | ReadStream
 }
 
 export const createCollection =
   (client: () => AxiosInstance, path: 'project' | 'playlist' | 'custom-tag') =>
   async (request: CreateRequest): Promise<Response<CollectionResponse>> => {
     try {
-      const response = await client().post(`/${API_VERSION}/${path}`, request)
+      const isImageUploaded = request?.image instanceof ReadStream
+      let payload: CreateRequest | FormData = request
+
+      if (isImageUploaded) {
+        const data = new FormData()
+        transformObjToFormData(data, request)
+        payload = data
+      }
+      
+      const response = await client().post(`/${API_VERSION}/${path}`, payload)
       const parserResponse = collectionResponseSchema.parse(response.data)
       return successResponse(parserResponse)
     } catch (error) {
