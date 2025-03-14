42ebaf05d186a541423d0fedba8f2d68fe6a835e
afatoga <afatoga@gmail.com>
2025-03-14 09:49:58 +0100

    tsup bundler, esm output

--- src/helpers/thirdPartyLink/utils.ts
+++ src/helpers/thirdPartyLink/utils.ts
@@ -1,10 +1,6 @@
 import { parse, toSeconds } from 'iso8601-duration'
 
 export const iso8601ToSeconds = (duration: string): number => toSeconds(parse(duration))
-export const isSupportedStreamingServiceLink = (link: string): boolean =>
-  link.match(
-    /(youtu\.be|youtube\.com|ytimg\.com|vimeo\.com|vimeocdn\.com|vimeopro\.com|tiktok\.com|open\.spotify\.com|soundcloud\.com|music\.apple\.com)/i,
-  ) !== null
 
 // Announcement: removes characters before link
 export const extractCleanLink = (text: string) => {
