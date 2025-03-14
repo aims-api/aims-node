ac526ceec1b0ec0ebbb189bf43aa68479dcb4d63
afatoga <afatoga@gmail.com>
2025-03-14 09:55:34 +0100

    import jest config

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,4 +1,4 @@
-import config from './jest.config'
+import config from './jest.config.js'
 config.testRegex = '/test/live_integration/.*\\.test\\.ts$'
 console.log('RUNNING INTEGRATION TESTS')
 export default config
