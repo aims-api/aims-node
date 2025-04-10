df4bd4c4bc62ad748fd1d96db50bf9df6f1bf100
afatoga <32682730+afatoga@users.noreply.github.com>
2025-04-10 14:34:24 +0200

    feat: search tracks post request (#78)
    * post request
    
    * test
    
    * ignore pck.json

---
+++ .changeset/quiet-toes-impress.md
@@ -0,0 +1,5 @@
+---
+"@aims-api/aims-node": patch
+---
+
+searchTracks method calls post
--- src/endpoints/track/search.ts
+++ src/endpoints/track/search.ts
@@ -21,7 +21,7 @@ const searchTracks =
   (client: () => AxiosInstance) =>
   async (request: Request): Promise<Response<TrackListResponse | TrackListDetailedResponse>> => {
     try {
-      const response = await client().get(`/${API_VERSION}/tracks/search`, { params: request })
+      const response = await client().post(`/${API_VERSION}/tracks/search`, request)
       if (response.status === 204) {
         return successResponse(emptyResultsResponse)
       }
